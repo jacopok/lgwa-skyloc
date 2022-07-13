795271fa8d0e9f0bef47da691a86ca4346ce710d
jacopok <jacopok@gmail.com>
2022-07-13 19:03:09 +0200

    feat: moved to GW170817 params for plot

--- plot_psds_bns.py
+++ plot_psds_bns.py
@@ -1,23 +1,27 @@
 import matplotlib.pyplot as plt
 from lgwa_skyloc.psds_against_bns_signal import plot_snr
 from lgwa_skyloc.white_dwarfs import FIGS
+import numpy as np
 
 if __name__ == '__main__':
     parameters = {
         'phase': 0.,
-        'geocent_time': 1577491218., # 1st of January 2030
+        # 'geocent_time': 1577491218., # 1st of January 2030
         'redshift': .01,
+        'mass_1': 1.4,
+        'mass_2': 1.4,
         'luminosity_distance': 40,
-        'theta_jn': 1.,
-        'mass_1': 1.5,
-        'mass_2': 1.5,
-        'ra': 0.,
-        'dec': 0.,
-        'psi': 0.,
-        'lambda_1': 500., 
-        'lambda_2': 500., 
-        'a_1': 0., 
-        'a_2': 0., 
+        'theta_jn': 5/6*np.pi,
+        'ra': 3.45,
+        'dec': -0.41,
+        'psi': 1.6 ,
+        'phase': 0 ,
+        'geocent_time': 1187008882, # GW170817 time
+        'lambda_1': 400,
+        'lambda_2': 400,
+        'a_1': 0,
+        'a_2': 0,
+
     }
 
     plot_snr(parameters)
