32631214a949054de8bc4a6f365093e7e4bb511d
jacopok <jacopok@gmail.com>
2022-07-17 13:36:25 +0200

    feat: seeing where the merger frequency lies

--- plot_psds_bns.py
+++ plot_psds_bns.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from lgwa_skyloc.psds_against_bns_signal import plot_snr
+from lgwa_skyloc.white_dwarfs import merger_gw_frequency, u
 from lgwa_skyloc.plotting import FIGS
 import numpy as np
 
@@ -41,3 +42,40 @@ if __name__ == '__main__':
 
     plot_snr(parameters, signal_name = 'GW150914', waveform_model='lalsim_IMRPhenomD')
     plt.savefig(FIGS / 'ET_LGWA_BBH_150914.pdf')
+
+    parameters = {
+        'redshift': 1,
+        'mass_1': 1e4,
+        'mass_2': 1e4,
+        'luminosity_distance': 6791,
+        'theta_jn': 5/6*np.pi,
+        'ra': 7/24 * 2 * np.pi,
+        'dec': -75 / 180 * np.pi / 2,
+        'psi': 1.6,
+        'phase': 0 ,
+        'geocent_time': 1126259462, # GW150914 time
+        'a_1': -.1,
+        'a_2': -.1,
+    }
+
+    plot_snr(parameters, signal_name = 'IMBH', waveform_model='lalsim_IMRPhenomD')
+    plt.savefig(FIGS / 'ET_LGWA_BBH_IMBH.pdf')
+
+    parameters = {
+        'redshift': .005,
+        'mass_1': .8,
+        'mass_2': .8,
+        'luminosity_distance': 22,
+        'theta_jn': 5/6*np.pi,
+        'ra': 7/24 * 2 * np.pi,
+        'dec': -75 / 180 * np.pi / 2,
+        'psi': 1.6,
+        'phase': 0 ,
+        'geocent_time': 1187008882, # GW170817 time
+        'a_1': -.1,
+        'a_2': -.1,
+    }
+
+    plot_snr(parameters, signal_name = 'BWD', waveform_model='lalsim_IMRPhenomD')
+    # plt.axvline(merger_gw_frequency(.8*u.Msun, .8*u.Msun).value)    
+    plt.savefig(FIGS / 'ET_LGWA_BBH_BWD.pdf')
